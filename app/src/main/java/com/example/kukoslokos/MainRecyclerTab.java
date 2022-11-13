1c30ffa744a7ce4103166a6b0e1a042d0a3d5344
Sergio <uo271563@uniovi.es>
2022-11-13 18:52:29 +0100

    funciona la botonera y mejor estructuracion de los fragments

--- app/src/main/java/com/example/kukoslokos/MainRecyclerTab.java
+++ app/src/main/java/com/example/kukoslokos/MainRecyclerTab.java
@@ -1,14 +1,20 @@
 package com.example.kukoslokos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.example.kukoslokos.model.Pelicula;
 import com.example.kukoslokos.model.Seccion;
 import com.example.kukoslokos.tasks.GetPelis;
+import com.example.kukoslokos.ui.HomeFragment;
+import com.example.kukoslokos.ui.ProfileFragment;
+import com.example.kukoslokos.ui.SavedFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,8 +25,7 @@ import java.util.concurrent.ExecutionException;
 
 public class MainRecyclerTab extends AppCompatActivity {
 
-    static final Map<String, String> SECCIONES = Map.of("Novedades", "now_playing", "Populares","popular", "Tendencias","top_rated", "Proximamente", "upcoming");
-    List<Seccion> seccionList;
+
 
 
     @Override
@@ -28,48 +33,52 @@ public class MainRecyclerTab extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_tab);
 
-        cargarSecciones();
+        //Gestion de la barra de navegacion
+        BottomNavigationView navView = findViewById(R.id.bottomNavigationView);
+        navView.setOnNavigationItemSelectedListener(navBarListener);
+        navView.setSelectedItemId(R.id.home);
+
+        int i=0;
     }
 
-    //Cargar secciones
-    private void cargarSecciones() {
-        HashMap<String, List<Pelicula>> peliculasEnSecciones = new HashMap<String, List<Pelicula>>();
-        for (String key: SECCIONES.keySet()){
-            peliculasEnSecciones.put(key, cargarPeliculas(SECCIONES.get(key)));
-        }
-        //Creamos batida de secciones estaticas
-        seccionList = createSections(peliculasEnSecciones);
-        //obterner linearlayout
-        RecyclerView seccionesView = findViewById(R.id.secctionList);
-        PeliculasAdapter.OnItemClickListener listener = new PeliculasAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(Pelicula peli) {
-                Log.i("listened", "Cambio de vista a DETALLES DE" + peli.getTitulo());
+    private BottomNavigationView.OnNavigationItemSelectedListener navBarListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.home:
+                    //Creamos el framento de información
+                    HomeFragment homeFragment = new HomeFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, homeFragment).commit();
+
+                    return true;
+
+                case R.id.saved:
+                    SavedFragment savedFragment = new SavedFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, savedFragment).commit();
+
+
+                    return true;
+
+                case R.id.profile:
+
+                    ProfileFragment profileFragment = new ProfileFragment();
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, profileFragment).commit();
+
+                    return true;
+
+                default:
+                    throw new IllegalStateException("Unexpected value: " + item.getItemId());
+
             }
-        };
-        seccionesView.setAdapter(new SectionAdapter(seccionList, listener));
 
-    }
-
-    private List<Seccion> createSections(HashMap<String, List<Pelicula>> peliculasEnSecciones) {
-        List<Seccion> secciones = new ArrayList<Seccion>();
-
-        for (String key : peliculasEnSecciones.keySet())
-            secciones.add(new Seccion(key, peliculasEnSecciones.get(key)));
-
-        return secciones;
-    }
-
-    private List<Pelicula> cargarPeliculas(String categoria) {
-        try {
-            GetPelis populares = new GetPelis(categoria);
-            populares.execute();
-            return populares.get();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
-        return new ArrayList<>();
-    }
+
+    };
+
+
+
 }
--- app/src/main/java/com/example/kukoslokos/ui/HomeFragment.java
+++ app/src/main/java/com/example/kukoslokos/ui/HomeFragment.java
@@ -3,12 +3,25 @@ package com.example.kukoslokos.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.kukoslokos.PeliculasAdapter;
 import com.example.kukoslokos.R;
+import com.example.kukoslokos.SectionAdapter;
+import com.example.kukoslokos.model.Pelicula;
+import com.example.kukoslokos.model.Seccion;
+import com.example.kukoslokos.tasks.GetPelis;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +38,9 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    static final Map<String, String> SECCIONES = Map.of("Novedades", "now_playing", "Populares","popular", "Tendencias","top_rated", "Proximamente", "upcoming");
+    List<Seccion> seccionList;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -55,6 +71,7 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
@@ -62,5 +79,53 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        cargarSecciones();
+    }
+
+    //Cargar secciones
+    private void cargarSecciones() {
+        HashMap<String, List<Pelicula>> peliculasEnSecciones = new HashMap<String, List<Pelicula>>();
+        for (String key: SECCIONES.keySet()){
+            peliculasEnSecciones.put(key, cargarPeliculas(SECCIONES.get(key)));
+        }
+        //Creamos batida de secciones estaticas
+        seccionList = createSections(peliculasEnSecciones);
+        //obterner linearlayout
+        RecyclerView seccionesView = getView().findViewById(R.id.secctionList);
+        PeliculasAdapter.OnItemClickListener listener = new PeliculasAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Pelicula peli) {
+                Log.i("listened", "Cambio de vista a DETALLES DE" + peli.getTitulo());
+            }
+        };
+        seccionesView.setAdapter(new SectionAdapter(seccionList, listener));
+
+    }
+
+    private List<Seccion> createSections(HashMap<String, List<Pelicula>> peliculasEnSecciones) {
+        List<Seccion> secciones = new ArrayList<Seccion>();
+
+        for (String key : peliculasEnSecciones.keySet())
+            secciones.add(new Seccion(key, peliculasEnSecciones.get(key)));
+
+        return secciones;
+    }
+
+    private List<Pelicula> cargarPeliculas(String categoria) {
+        try {
+            GetPelis populares = new GetPelis(categoria);
+            populares.execute();
+            return populares.get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
     }
 }
