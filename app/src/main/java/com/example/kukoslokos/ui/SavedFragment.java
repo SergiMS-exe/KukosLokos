fb3a509a094c9c8eee803e6d34f823e08aa0dbfd
Sergio <uo271563@uniovi.es>
2022-11-20 16:27:33 +0100

    Se ven las peliculas guardadas

--- app/src/main/java/com/example/kukoslokos/ui/SavedFragment.java
+++ app/src/main/java/com/example/kukoslokos/ui/SavedFragment.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
